2a57c4c3aa8ef61653858ff5b6d6c935272111e9
Electr1k <gynter37@gmail.com>
2024-04-20 20:58:27 +0300

    add auth with google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     //noinspection GradleCompatible
     implementation("com.android.support:cardview-v7:21.+")
     implementation ("de.hdodenhof:circleimageview:2.1.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -62,4 +63,5 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:21.1.0")
 }
--- app/src/main/java/ru/trifonov/clubfirst/fragment/AuthFragment.kt
+++ app/src/main/java/ru/trifonov/clubfirst/fragment/AuthFragment.kt
@@ -1,5 +1,6 @@
 package ru.trifonov.clubfirst.fragment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +9,19 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import ru.trifonov.clubfirst.R
 
 
@@ -18,6 +30,7 @@ class AuthFragment : Fragment() {
     private lateinit var authBtn: CardView
     private lateinit var textTop:TextView
     private lateinit var textBottom:TextView
+    val auth = FirebaseAuth.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,11 +43,50 @@ class AuthFragment : Fragment() {
         authBtn = view.findViewById(R.id.authBtn)
         textTop = view.findViewById(R.id.top_text)
         textBottom = view.findViewById(R.id.bottom_text)
-
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestId().requestEmail().build()
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
         authBtn.setOnClickListener {
-            findNavController().navigate(R.id.action_auth_to_registration)
+            launcher.launch(googleSignInClient.signInIntent)
         }
 
+    }
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+        else{
+            Toast.makeText(requireContext(), "Упс... Произошла ошибка, проверьте соединение с Интернетом", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>){
+        if (task.isSuccessful){
+            val account: GoogleSignInAccount? = task.result
+            if (account != null){
+                val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                auth.signInWithCredential(credential).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        println(it.result.user)
+                        findNavController().navigate(R.id.action_auth_to_registration)
+                    }
+                }
+            }
+            else{
+                Toast.makeText(requireContext(), "Упс... Произошла ошибка, проверьте соединение с Интернетом", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        else{
+            Toast.makeText(requireContext(), "Упс... Произошла ошибка, проверьте соединение с Интернетом", Toast.LENGTH_SHORT).show()
+
+        }
     }
 
     override fun onStart() {
