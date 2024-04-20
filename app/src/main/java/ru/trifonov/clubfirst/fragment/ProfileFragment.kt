32bc401599fc4f70400755e3ae77e50de75973af
Electr1k <gynter37@gmail.com>
2024-04-20 15:01:14 +0300

    remake profile

--- app/src/main/java/ru/trifonov/clubfirst/fragment/ProfileFragment.kt
+++ app/src/main/java/ru/trifonov/clubfirst/fragment/ProfileFragment.kt
@@ -5,25 +5,83 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import org.w3c.dom.Text
 import ru.trifonov.clubfirst.R
 import ru.trifonov.clubfirst.databinding.ProfileFragmentBinding
 
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var bind: ProfileFragmentBinding
-
+    private lateinit var profileFragment: FrameLayout
+    private lateinit var cardImage: CardView
+    private lateinit var cardInfo: CardView
+    private lateinit var mBottomSheet: LinearLayout
+    private lateinit var mBottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+    private lateinit var moreBtn: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        bind = ProfileFragmentBinding.inflate(inflater, container, false)
-        //bind.
-        return bind.root
+        return inflater.inflate(R.layout.profile_fragment, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        profileFragment = view.findViewById(R.id.profile_fragment)
+        moreBtn = view.findViewById(R.id.moreBtn)
+        cardImage = view.findViewById(R.id.card_image)
+        cardInfo = view.findViewById(R.id.card_info)
+        mBottomSheet = view.findViewById(R.id.bottom_sheet)
+        mBottomSheetBehavior =  BottomSheetBehavior.from(mBottomSheet)
+        mBottomSheetBehavior.isHideable = false
 
+        moreBtn.setOnClickListener {
+            if (moreBtn.text == "Подробнее"){
+
+                cardInfo.findViewById<TextView>(R.id.description).maxLines = 1000
+                mBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                moreBtn.text = "Свернуть"
+            }
+            else{
+                cardInfo.findViewById<TextView>(R.id.description).maxLines = 2
+                mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                moreBtn.text = "Подробнее"
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var startTranslation = 0f
+        cardImage.post{
+            val cardHeight = cardImage.height + cardImage.translationY
+            startTranslation = cardImage.translationY
+            mBottomSheetBehavior.peekHeight = (profileFragment.height - cardHeight + cardInfo.height * 0.5).toInt()
+            mBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+
+        }
+        mBottomSheetBehavior.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                println(slideOffset)
+                cardImage.translationY = startTranslation - bottomSheet.height * slideOffset * 0.2f
+                val image = cardImage.findViewById<ImageView>(R.id.image_profile)
+                image.scaleX = 1 + 0.2f * slideOffset
+                image.scaleY = 1 + 0.2f * slideOffset
+            }
+
+        })
     }
 }
